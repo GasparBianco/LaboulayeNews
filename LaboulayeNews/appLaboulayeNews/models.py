b0a5e86f23c611363ae7e9824618753a9cc06b6c
Gaspar Bianco <gasparbianco98@gmail.com>
2023-10-04 19:46:15 -0300

    imagenes

--- LaboulayeNews/appLaboulayeNews/models.py
+++ LaboulayeNews/appLaboulayeNews/models.py
@@ -14,7 +14,7 @@ class News(models.Model):
     author = models.CharField(max_length=255, default='Anonimo')
     date = models.DateTimeField(auto_now_add=True)
     body = models.TextField(blank=False, null=False)
-    #image = models.ImageField(upload_to='noticias/', null=True, blank=True)
+    image = models.ImageField(upload_to='media/news/', null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     sinopsis = models.TextField(blank=False, null=False)
 
--- LaboulayeNews/appLaboulayeNews/urls.py
+++ LaboulayeNews/appLaboulayeNews/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('<int:current_page>/', views.home, name = 'home'),
     path('', views.home, name = 'home'),
@@ -8,3 +9,6 @@ urlpatterns = [
     path('news/<int:id>/', views.news, name='news'),
     path('contact/', views.contact, name='contact')
     ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
